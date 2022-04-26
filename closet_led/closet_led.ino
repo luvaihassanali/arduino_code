16dba430e6534f33bde74bdef1bf829a08c2313d
Luvai Hassanali <luvaihassanali@gmail.com>
2022-04-25 23:42:31 -0500

    low power mode

--- closet_led/closet_led.ino
+++ closet_led/closet_led.ino
@@ -1,50 +1,31 @@
-
 #include "PinDefinitionsAndMore.h"
 #include <IRremote.h>
+#include "LowPower.h"
 
-const int IR_SENSOR_PIN = 2;
 const uint8_t LED_CMD = 0x80;
 const uint8_t LED_ON = 0x12;
 const uint8_t LED_OFF = 0x1A;
-const bool debugLog  = true;
+const byte IR_SENSOR_PIN = 2;
+byte irInteruptState = 0;
 
-bool ledOn = false;
+void irInterupt() {}
 
-void setup() {
+void setup()
+{
   pinMode(IR_SENSOR_PIN, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  if (debugLog) {
-    Serial.begin(9600);
-    Log("Ready");
-  }
+  IrSender.sendNEC(LED_CMD, LED_OFF, 1);
 }
 
-void loop() {
-  if (digitalRead(IR_SENSOR_PIN) == HIGH) {
-    Log("High");
-    if (!ledOn) {
-      Log("in high");
-      IrSender.sendNEC(LED_CMD, LED_ON, 1);
-      ledOn = true;
-    }
+void loop()
+{
+  attachInterrupt(0, irInterupt, CHANGE);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  detachInterrupt(0);
+
+  irInteruptState = digitalRead(IR_SENSOR_PIN);
+  if (irInteruptState == HIGH) {
+    IrSender.sendNEC(LED_CMD, LED_ON, 1);
   } else {
-    Log("Low");
-    if (ledOn) {
-      Log("in low");
-      IrSender.sendNEC(LED_CMD, LED_OFF, 1);
-      ledOn = false;
-    }
+    IrSender.sendNEC(LED_CMD, LED_OFF, 1);
   }
 }
-
-void Log(String msg) {
-  if (debugLog) {
-    Serial.println(msg);
-  }
-}
-/*
-ON
-23:53:06.552 -> Protocol=NEC Address=0x80 Command=0x12 Raw-Data=0xED127F80 32 bits LSB first
-OFF
-23:53:12.448 -> Protocol=NEC Address=0x80 Command=0x1A Raw-Data=0xE51A7F80 32 bits LSB first
-*/
