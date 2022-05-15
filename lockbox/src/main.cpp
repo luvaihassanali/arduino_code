7648028dcd915d9a810f45897b4dd834eca2d60a
Luvai Hassanali <luvaihassanali@gmail.com>
2022-05-15 06:06:52 -0400

    add ir sensor lockbox

--- lockbox/src/main.cpp
+++ lockbox/src/main.cpp
@@ -4,9 +4,8 @@
 #define DEBUG true
 #define FULLSTEP 4
 
-// const int TILT_SENSOR_PIN = A0;
 const int BUTTON_PIN = 4;
-// const int IR_SENSOR_PIN = 2;
+const int IR_SENSOR_PIN = 5;
 bool forward = true;
 AccelStepper myStepper(FULLSTEP, 8, 10, 9, 11);
 
@@ -19,6 +18,7 @@ void setup()
     Serial.begin(9600);
   }
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(IR_SENSOR_PIN, INPUT);
   myStepper.setMaxSpeed(1000.0);
   myStepper.setAcceleration(100.0);
   myStepper.setSpeed(250);
@@ -27,19 +27,24 @@ void setup()
 
 void loop()
 {
-  int sensorVal = digitalRead(BUTTON_PIN);
 
-  if (sensorVal == LOW)
+  int buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState == LOW)
   {
     Log("Button pushed " + String(forward));
-    if (forward) {
+    if (forward)
+    {
       forward = false;
-      myStepper.moveTo(300);
-    } else {
+      myStepper.moveTo(310);
+    }
+    else
+    {
       forward = true;
       myStepper.moveTo(0);
     }
     delay(500);
+    int irState = digitalRead(IR_SENSOR_PIN);
+    Log("IR sensor: " + String(irState));
   }
 
   myStepper.run();
