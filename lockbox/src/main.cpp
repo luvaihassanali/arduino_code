60d77d5c76729e7b137cb83638da6e1b24958510
Luvai Hassanali <luvaihassanali@gmail.com>
2022-05-19 07:00:05 -0400

    switch to servo

--- lockbox/src/main.cpp
+++ lockbox/src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
-#include <AccelStepper.h>
+#include <Servo.h>
+//#include <arduino-timer.h>
 
 #define DEBUG true
-#define FULLSTEP 4
 
-const int BUTTON_PIN = 4;
-const int IR_SENSOR_PIN = 5;
-bool forward = true;
-AccelStepper myStepper(FULLSTEP, 8, 10, 9, 11);
+const int IR_SENSOR_PIN = 7;
+// const int TICK_LENGTH = 5000;
 
+Servo servo;
+
+// auto timer = timer_create_default();
+// bool locked = true;
+bool open = false;
+int servoPos = 0;
+
+// void Lock();
 void Log(String msg);
+// bool Unlock(void *);
 
 void setup()
 {
@@ -17,37 +24,78 @@ void setup()
   {
     Serial.begin(9600);
   }
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
   pinMode(IR_SENSOR_PIN, INPUT);
-  myStepper.setMaxSpeed(1000.0);
-  myStepper.setAcceleration(100.0);
-  myStepper.setSpeed(250);
-  Log("Setup done");
+  // pinMode(LED_BUILTIN, OUTPUT);
+
+  servo.write(0);
+  servo.attach(2);
+  Log("setup done");
+
+  // Log("set timer tick: " + String(TICK_LENGTH));
+  // timer.every(TICK_LENGTH, Unlock); // 7200000
+  // digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {
 
-  int buttonState = digitalRead(BUTTON_PIN);
-  if (buttonState == LOW)
+  int irState = digitalRead(IR_SENSOR_PIN);
+  if (irState == 0)
   {
-    Log("Button pushed " + String(forward));
-    if (forward)
+    if (!open)
     {
-      forward = false;
-      myStepper.moveTo(310);
+      Log("IR sensor: " + String(irState));
+      for (servoPos = 0; servoPos <= 180; servoPos += 1)
+      {
+        // in steps of 1 degree
+        servo.write(servoPos);
+        delay(15);
+      }
+      Log("moveto 310");
+      open = true;
+      delay(100);
+    }
+  }
+  else
+  {
+    if (open)
+    {
+      Log("IR sensor: " + String(irState));
+      for (servoPos = 180; servoPos >= 0; servoPos -= 1)
+      {
+        servo.write(servoPos);
+        delay(15);
+      }
+      Log("moveto 0");
+      open = false;
+      delay(100);
+    }
+  }
+
+  /*if (!locked)
+  {
+    int irState = digitalRead(IR_SENSOR_PIN); // 1 is open 0 is closed
+    if (!open)
+    {
+      if (irState == 1)
+      {
+        Log("open = true, IR sensor: " + String(irState));
+        open = true;
+        //delay(5000);
+      }
     }
     else
     {
-      forward = true;
-      myStepper.moveTo(0);
+      if (irState == 0)
+      {
+        Log("open = false, IR sensor: " + String(irState));
+        open = false;
+        //delay(1000);
+        Lock();
+      }
     }
-    delay(500);
-    int irState = digitalRead(IR_SENSOR_PIN);
-    Log("IR sensor: " + String(irState));
-  }
-
-  myStepper.run();
+  }*/
 }
 
 void Log(String msg)
@@ -57,3 +105,22 @@ void Log(String msg)
     Serial.println(msg);
   }
 }
+/*
+void Lock()
+{
+  Log("lock");
+  locked = true;
+  digitalWrite(LED_BUILTIN, LOW);
+  stepper.moveTo(END_POS);
+  Log("set timer tick: " + String(TICK_LENGTH));
+  //timer.every(TICK_LENGTH, Unlock);
+}
+
+bool Unlock(void *)
+{
+  Log("unlock");
+  locked = false;
+  digitalWrite(LED_BUILTIN, HIGH);
+  stepper.moveTo(0);
+  return false;
+}*/
